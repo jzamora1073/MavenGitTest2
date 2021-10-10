64d22d81098b92118ceaacf906b28b2cf1d185ac
Zamora <jzamora1073@gmail.com>
2021-10-10 19:09:07 -0400

    New Work added

---
+++ src/main/java/HelloMain.java
@@ -0,0 +1,6 @@
+public class HelloMain {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
         System.out.println("Hello Word");
 
         System.out.println(" Work 1");
+        System.out.println("Local repo is published to GitHub");
 
     }
 }
